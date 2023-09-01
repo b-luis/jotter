f73fc1aa6eef5bc63bb4cacea645bec2a52a1ebe
b-luis <bscarletluis@gmail.com>
2023-09-02 01:44:55 +0800

    feat: Start component

--- jotter/src/App.jsx
+++ jotter/src/App.jsx
@@ -2,14 +2,10 @@ import { useState, useEffect } from "react";
 import { Start, Header, Notes, SelectedNote } from "./components";
 
 const App = () => {
-  return (
-    <>
-      <Start />
-      <Header />
-      <Notes />
-      <SelectedNote />
-    </>
-  );
+	const [start, setStart] = useState(false);
+	const [notes, setNotes] = useState("");
+	
+	return <main>{start ? <Header /> : <Start onClick={() => setStart(!start)} />}</main>;
 };
 
 export default App;
--- jotter/src/components/Landing/Start.jsx
+++ jotter/src/components/Landing/Start.jsx
@@ -1,9 +1,22 @@
-function Start() {
-  return (
-    <>
-      <h1>Start of Landing Page Here</h1>
-    </>
-  );
+import logo from "../../assets/night-mode.png";
+
+function Start({ onClick }) {
+	return (
+		<div className="align-center flex h-screen flex-col justify-center p-5 text-center">
+			<img className="mx-auto object-contain" src={logo} alt="Jotter Logo" />
+			<p className="text-gray text-gray-200 sm:text-2xl md:text-3xl lg:text-4xl">
+				Where ideas take shape, one note at a time.
+			</p>
+			<button
+				onClick={onClick}
+				className="m-10 mx-auto w-[120px] rounded-xl bg-gradient-to-r from-gray-200 to-slate-400 px-5 py-2 opacity-80 drop-shadow-lg backdrop-blur-xl md:w-[200px] md:py-3 md:text-xl"
+			>
+				<span className="bg-gradient-to-r from-slate-400 to-slate-700 bg-clip-text text-transparent hover:text-black">
+					Scribble
+				</span>
+			</button>
+		</div>
+	);
 }
 
 export default Start;
